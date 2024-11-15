77d1db564b421603c86a502be6ae5f90495be48c
franiart <89811759+franiart@users.noreply.github.com>
2024-11-15 17:58:08 -0500

    update character.ts

--- src/models/character.ts
+++ src/models/character.ts
@@ -1,3 +1,4 @@
+// CLASE BASE PARA TODOS LOS PERSONAJES
 class Character {
     name: string;
     level: number;
@@ -6,29 +7,41 @@ class Character {
     skills: string[];
     inventory: string[];
     
-    constructor(name: string, level: number, health: number, skills: string[]) {
+    constructor(name: string, health: number, skills: string[]) {
         this.name = name;
-        this.level = level;
+        this.level = 1;
         this.health = health;
         this.experience = 0;
         this.skills = skills;
         this.inventory = [];
     }
 
-    getExperience(): number {
-        return this.experience;
+    /**
+     * EL PERSONAJE GANA EXPERIENCIA Y CUANDO ALCANZA EL NIVEL MÁXIMO, SUBE DE LVL AUTOMÁTICAMENTE
+     * @param amount CANTIDAD DE EXPERIENCIA GANADA
+     */
+
+    gainExperience(amount: number): void {
+        this.experience += amount;
+        while (this.experience >= this.getNextLevelThreshold()) {
+            this.levelUp();
+        }
     }
 
-    getInventory():String[] {
-        return this.inventory;
+    /**
+     * SUBIR DE LVL INCREMENTA ATRIBUTOS Y AJUSTA LA EXPERIENCIA
+     */
+    private levelUp(): void {
+        this.level += 1;
+        this.health += 20;
+        console.log(`${this.name} ha subido al nivel ${this.level}!`);
     }
-
-    setExperience(exp: number): void {
-        this.experience = exp;
+    
+    /**
+     * 
+     * @returns CÁLCULO DEL MÁXIMO NECESARIO PARA PASAR AL SIGUIENTE LVL
+     */
+    private getNextLevelThreshold(): number {
+        return this.level * 100;
     }
-
-    setInventory(inv: []): void {
-        this.inventory = inv;
-    }
-
 }
