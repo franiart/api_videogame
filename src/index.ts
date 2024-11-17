198b1aef98cb1aadacbd6bdb5a44060f1c678085
franiart <89811759+franiart@users.noreply.github.com>
2024-11-16 21:10:21 -0500

    update index.ts: change valor character and mission

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,8 @@
+<<<<<<< Updated upstream
+=======
+<<<<<<< Updated upstream
+=======
+>>>>>>> Stashed changes
 import { createCharacter, listCharacters } from "./controllers/characterController";
 import { Mission, MissionType } from "./models/mission";
 import { triggerRandomEvent } from "./services/eventService";
@@ -6,29 +11,51 @@ import { triggerRandomEvent } from "./services/eventService";
 console.log("👾 ¡Bienvenido a Monstruópolis! 👾");
 
 // Crear personajes
+<<<<<<< Updated upstream
 const warrior = createCharacter("spectralWarrior", "Tharok", [], 50);
 const mage = createCharacter("darkWizard", "Lyra", [], 50);
 
 // Crear misiones
 const mission1 = new Mission("Explorar el Bosque Olvidado", 1, 100, MissionType.Side);
 const mission2 = new Mission("Defender la Torre de Cristal", 2, 200, MissionType.Main);
+=======
+const spectralWarrior = createCharacter("spectralWarrior", "Morrogallo", [], 50);
+const darkWizard = createCharacter("darkWizard", "Perrijime", [], 50);
+
+// Crear misiones
+const mission1 = new Mission("Explorar el Bosque de los Recuerdos", 1, 100, MissionType.Side);
+const mission2 = new Mission("Explorar el Cementerio de Almas", 2, 200, MissionType.Main);
+>>>>>>> Stashed changes
 
 // Mostrar personajes actuales
 console.log("🎭 Personajes creados:");
 console.table(listCharacters());
 
 // Asignar misiones
+<<<<<<< Updated upstream
 console.log(`📜 Asignando misión "${mission1.description}" a ${warrior.name}...`);
 warrior.inventory.push(mission1.description);
 
 console.log(`📜 Asignando misión "${mission2.description}" a ${mage.name}...`);
 mage.inventory.push(mission2.description);
+=======
+console.log(`📜 Asignando misión "${mission1.description}" a ${spectralWarrior.name}...`);
+spectralWarrior.inventory.push(mission1.description);
+
+console.log(`📜 Asignando misión "${mission2.description}" a ${darkWizard.name}...`);
+darkWizard.inventory.push(mission2.description);
+>>>>>>> Stashed changes
 
 // Simulación de eventos aleatorios
 console.log("🎲 Simulando eventos aleatorios...");
 Promise.all([
+<<<<<<< Updated upstream
     triggerRandomEvent(warrior),
     triggerRandomEvent(mage),
+=======
+    triggerRandomEvent(spectralWarrior),
+    triggerRandomEvent(darkWizard),
+>>>>>>> Stashed changes
 ]).then(() => {
     console.log("🎮 Eventos finalizados.");
 
@@ -40,4 +67,9 @@ Promise.all([
     });
 
 // Fin del juego
+<<<<<<< Updated upstream
 console.log("👾 ¡Gracias por jugar a Monstruópolis! 👾");
+=======
+console.log("👾 ¡Gracias por jugar a Monstruópolis! 👾");
+>>>>>>> Stashed changes
+>>>>>>> Stashed changes
