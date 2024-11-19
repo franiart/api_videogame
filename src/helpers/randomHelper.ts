f3c5b3732c592e6536eaf95e5d7b006f3c659cc7
franiart <89811759+franiart@users.noreply.github.com>
2024-11-19 13:59:39 -0500

    Update randomHelper.ts

--- src/helpers/randomHelper.ts
+++ src/helpers/randomHelper.ts
@@ -1,6 +1,6 @@
 /**
  * FUNCIÓN PARA DEVOLVER UN ELEMENTO DE LA LISTA
- * @param list Lista de elementos.
+ * @param list LISTA DE LOS ELEMENTOS
  */
 export function getRandomElement<element>(list: element[]): element {
     const index = Math.floor(Math.random() * list.length);
